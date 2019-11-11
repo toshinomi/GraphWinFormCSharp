6d16d154e2b951b1e6bfe7a8b1ccc29932f2c578
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-11-11 22:07:37 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GraphWinFormCSharp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView = new OxyPlot.WindowsForms.PlotView();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSVFileReadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plotView
+            // 
+            this.plotView.Location = new System.Drawing.Point(0, 27);
+            this.plotView.Name = "plotView";
+            this.plotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView.Size = new System.Drawing.Size(800, 421);
+            this.plotView.TabIndex = 1;
+            this.plotView.Text = "plotView";
+            this.plotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.Black;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileFToolStripMenuItem
+            // 
+            this.fileFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cSVFileReadToolStripMenuItem});
+            this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+            this.fileFToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fileFToolStripMenuItem.Text = "File(&F)";
+            // 
+            // cSVFileReadToolStripMenuItem
+            // 
+            this.cSVFileReadToolStripMenuItem.Name = "cSVFileReadToolStripMenuItem";
+            this.cSVFileReadToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.cSVFileReadToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.cSVFileReadToolStripMenuItem.Text = "CSV File Read(&O)";
+            this.cSVFileReadToolStripMenuItem.Click += new System.EventHandler(this.OnClickMenu);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.plotView);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private OxyPlot.WindowsForms.PlotView plotView;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cSVFileReadToolStripMenuItem;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace GraphWinFormCSharp
+{
+    public partial class FormMain : Form
+    {
+        private int[] m_PlotData = new int[256];
+        private Point m_mousePoint;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            menuStrip.MouseDown += new MouseEventHandler(OnMouseDownMenuStrip);
+            menuStrip.MouseMove += new MouseEventHandler(OnMouseMoveMenuStrip);
+
+            plotView.Model = new OxyPlot.PlotModel { PlotType = OxyPlot.PlotType.XY };
+
+            InitGraph();
+        }
+
+        private void OnMouseDownMenuStrip(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                m_mousePoint = new Point(e.X, e.Y);
+            }
+
+            return;
+        }
+
+        private void OnMouseMoveMenuStrip(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - m_mousePoint.X;
+                this.Top += e.Y - m_mousePoint.Y;
+            }
+
+            return;
+        }
+
+        private void InitGraph()
+        {
+            InitPlotData();
+
+            Draw();
+
+            return;
+        }
+
+        private void Draw()
+        {
+            var dataList = new List<DataPoint>();
+            for (int nIdx = 0; nIdx < m_PlotData.Length; nIdx++)
+            {
+                var dataPoint = new DataPoint(nIdx, m_PlotData[nIdx]);
+                dataList.Add(dataPoint);
+            }
+
+            var series = new LineSeries
+            {
+                ItemsSource = dataList,
+            };
+            var plotModel = new PlotModel();
+            plotModel.Series.Add(series);
+
+            plotView.Model.Series.Clear();
+            plotView.Model = plotModel;
+
+            return;
+        }
+
+        private void InitPlotData()
+        {
+            for (int nIdx = 0; nIdx < m_PlotData.Length; nIdx++)
+            {
+                m_PlotData[nIdx] = 0;
+            }
+
+            return;
+        }
+
+        private void DrawPlotData()
+        {
+            ComOpenFileDialog openFileDlg = new ComOpenFileDialog();
+            openFileDlg.Filter = "CSV|*.csv";
+            openFileDlg.Title = "Open the file";
+            if (openFileDlg.ShowDialog() == true)
+            {
+                ReadCsv(openFileDlg.FileName);
+                Draw();
+            }
+            return;
+        }
+
+        private void OnClickMenu(object sender, EventArgs e)
+        {
+            string strHeader = sender.ToString();
+
+            switch (strHeader)
+            {
+                case "CSV File Read(&O)":
+                    {
+                        DrawPlotData();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ReadCsv(string _strFileName)
+        {
+            StreamReader stream = new StreamReader(_strFileName);
+            {
+                while (!stream.EndOfStream)
+                {
+                    string strLine = stream.ReadLine();
+                    string[] strValues = strLine.Split(',');
+
+                    int nIndex = int.Parse(strValues.GetValue(0).ToString());
+                    int nValue = int.Parse(strValues.GetValue(1).ToString());
+                    if (nIndex > 255)
+                    {
+                        break;
+                    }
+                    m_PlotData[nIndex] = nValue;
+                }
+            }
+
+            return;
+        }
+    }
+}
